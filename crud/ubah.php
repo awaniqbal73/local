8c4e45310b5a0ae18a264606706bddef30be59a5
awaniqbal73 <awaniqbal73@gmail.com>
2019-02-26 11:24:38 +0700

    134
    123

---
+++ crud/ubah.php
@@ -0,0 +1,89 @@
+<?php
+
+require'functions.php';
+
+
+//ambil data di URL 
+$no=$_GET["no"];
+
+//Query data berdasarkan nomer
+$mhs=  query ("SELECT * FROM tabelbelajar  WHERE no = $no")[0];
+
+
+//cek apakah tomboh submit sudah ditekan atau belum
+if(isset($_POST["submit"])){
+
+    
+
+
+    //cek data berhasih di Ubah atau tidak
+  if( ubah($_POST) > 0) {
+        echo"
+            <script>
+            alert('data berhasil ubah');
+            document.location.href='index.php';
+            </script>
+        ";
+  } else {
+      echo "
+            <script>
+            alert('data gagal ubah');
+            document.location.href='ubah.php';
+            </script>
+      ";
+  }
+    
+  }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ubah data Mahasiswa</title>
+</head>
+<body>
+    <h1> Tmbah Data  </h1>
+    <form action="" method="post">
+        
+        <input type="hidden" name="no" value="<?= $mhs["no"];?>">
+        <ul>
+            <li>
+                <label for="gambar">gambar</label>
+                <input type="text" name="gambar" id="gambar" 
+                value="<?=$mhs["gambar"]; ?>">
+            </li>
+            <li>
+                <label for="nrp"> NRP</label>
+                <input type="text" name="nrp" id="nrp" 
+                value="<?=$mhs["nrp"]; ?>">
+            </li>
+            <li>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama"
+                value="<?=$mhs["nama"]; ?>">
+            </li>
+            <li>
+                <label for="email"> Email</label>
+                <input type="text" name="email" id="email"
+                value="<?=$mhs["email"]; ?>">
+            </li>
+            <li>
+                <label for="jurusan"> Jurusan</label>
+                <input type="text" name="jurusan" id="jurusan"
+                value="<?=$mhs["jurusan"]; ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data</button>
+            </li>
+        </ul>
+
+
+    </form>
+
+
+</body>
+</html>
--- pertemuan_13/functions.php
+++ pertemuan_13/functions.php
@@ -18,7 +18,11 @@ function query($query) {
 
 function tambah($data){
     global $conn;
-    $gambar = htmlspecialchars($data["gambar"]);
+    //UPLOD GAMBAR
+    $gambar = upload();
+    if (!$gambar){
+        return false;
+    }
     $nrp = htmlspecialchars($data["nrp"]);
     $nama = htmlspecialchars($data["nama"]);
     $email = htmlspecialchars($data["email"]);
@@ -42,12 +46,67 @@ function delete($no) {
     return mysqli_affected_rows($conn);
 }
 
+function upload(){
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    //cek apakah tidak ada gambar yang di upload
+    if($error === 4) {
+        echo "<script>
+            alert('pilih gambar terlebih dahulu');
+            </script>";
+        return false;
+    }
+
+    //cek apakah yang di upload gambar
+    $ekstensiGambarValid =['jpg','jpeg','png'];
+    $ekstensiGambar = explode ('.',$namaFile);
+    $ekstensiGambar = strtolower (end($ekstensiGambar));
+
+    if( !in_array($ekstensiGambar, $ekstensiGambarValid)){
+        echo "<script>
+        alert('yang di upload bukan gambar');
+        </script>";
+    return false;
+    }
+
+
+    //cek jika ukuran file gambar erlalu besar
+    if( $ukuranFile > 1000000){
+        echo "<script>
+        alert('ukuran terlalu besar');
+        </script>";
+    return false;
+    }
+
+    //lolos pengecekan, gambar siap di upload
+    //generate nama file baru
+    $namaFileBaru = uniqid();
+    $namaFileBaru .='.';
+    $namaFileBaru .= $ekstensiGambar;
+   
+
+
+    move_uploaded_file($tmpName, 'img/'. $namaFileBaru);
+    return $namaFileBaru;
+
+}
+
 
 
 function ubah($data){
     global $conn;
     $no = $data["no"];
-    $gambar = htmlspecialchars($data["gambar"]);
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+    //cek apakah usert memilih gambar baru atau tidak
+    if($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    } else {
+        $gambar = upload();
+    }
+
     $nrp = htmlspecialchars($data["nrp"]);
     $nama = htmlspecialchars($data["nama"]);
     $email = htmlspecialchars($data["email"]);
